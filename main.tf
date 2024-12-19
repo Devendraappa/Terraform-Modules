253e715c36b4e7e97f3424750f1b836068a58fb7
Devendraappa <devendraappa.kune@gmail.com>
2024-12-19 08:51:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,21 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./terraform-modules/vpc"
+  source = "./terraform-vpc-module"
 
   region              = "ap-south-1"
   vpc_cidr            = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   availability_zones  = ["ap-south-1a", "ap-south-1b"]
+  vpc_name            = "my-vpc"
 }
 
 output "vpc_id" {
   value = module.vpc.vpc_id
+}
+
+output "vpc_name" {
+  value = module.vpc.vpc_name
 }
 
 output "public_subnet_ids" {
