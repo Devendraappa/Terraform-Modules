7972c602c3c62bb3d4589c44eac019a8bb018682
Devendraappa <devendraappa.kune@gmail.com>
2024-12-19 08:50:29 +0530

    Update main.tf

--- terraform-modules/vpc/main.tf
+++ terraform-modules/vpc/main.tf
@@ -4,6 +4,10 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -11,14 +15,26 @@ resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = element(var.public_subnet_cidrs, count.index)
   availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "${var.vpc_name}-public-${count.index + 1}"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
 }
 
 resource "aws_route" "internet_access" {
