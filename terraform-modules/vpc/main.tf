bfdcd5b8d9ecc5d3137f6953dce2613f21e70eb4
Devendraappa <devendraappa.kune@gmail.com>
2024-12-19 08:40:34 +0530

    Update main.tf

--- terraform-modules/vpc/main.tf
+++ terraform-modules/vpc/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
@@ -5,11 +9,11 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   count             = length(var.public_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidrs[count.index]
-  map_public_ip_on_launch = true
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
 }
 
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
@@ -17,14 +21,14 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route" "default_route" {
+resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnet_cidrs)
-  subnet_id      = aws_subnet.public[count.index].id
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
   route_table_id = aws_route_table.public.id
 }
