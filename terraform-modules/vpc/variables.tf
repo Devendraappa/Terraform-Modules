686dbe4fc9933a3ef40c5fc1badc54b88a929d55
Devendraappa <devendraappa.kune@gmail.com>
2024-12-19 08:40:54 +0530

    Update variables.tf

--- terraform-modules/vpc/variables.tf
+++ terraform-modules/vpc/variables.tf
@@ -1,9 +1,23 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "region" {
+  description = "The AWS region to create resources in"
   type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of public subnet CIDR blocks"
+  description = "A list of CIDR blocks for public subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  description = "A list of availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
 }
